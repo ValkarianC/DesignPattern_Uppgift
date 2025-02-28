d033bbc0c8f65e6a5941fd99aef2c4429fba34f8
BP <valkarian@valkarian.com>
2025-02-28 10:04:50 +0100

    Task: Added Address for use with customer class

---
+++ src/main/java/org/example/Model/Address.java
@@ -0,0 +1,70 @@
+package org.example.Model;
+
+public class Address {
+    private String addressLine1;
+    private String addressLine2;
+    private String town;
+    private String county;
+    private String country;
+    private String postcode;
+
+    public Address() {
+    }
+
+    public Address(String addressLine1, String addressLine2, String town, String county, String country, String postcode) {
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.town = town;
+        this.county = county;
+        this.country = country;
+        this.postcode = postcode;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+}
